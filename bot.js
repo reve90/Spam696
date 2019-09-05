d857ac0509be490e559f68dc50c2c3317d5cf5ca
reve90 <54951008+reve90@users.noreply.github.com>
2019-09-05 18:25:34 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("619199147358158848")
+setInterval(function() {
+channel.send(Set Set St Lacha Crystal);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
